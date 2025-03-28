ab5d961a7fa1ca3167e5dee50bcf34a94ea95426
laicapseivan <ivanvsgoncalves@gmail.com>
2025-03-28 14:23:24 +0000

    densidadeecapita

--- super_trunfo.c
+++ super_trunfo.c
@@ -3,30 +3,37 @@
 int main(){
 
     //Declaração de variáveis: Carta 1
-    //Declaração de variáveis do tipo char (Strings ou caracteres únicos)
-    char estado1 = ' ', codigo1[4] = "A01", cidade1[20] = " ";
-    //Declaração de variáveis do tipo int (Números inteiros)
-    int populacao1 = 0, pontoturistico1 = 0;
-    //Declaração de variáveis do tipo float (Números de ponto flutuante)
-    float areakm1 = 0.0, pib1 = 0.0;
+
+    //Declaração de variáveis tipo char 
+    char estado1, codigo1[4], cidade1[20];
+
+    //Declaração de variáveis tipo int 
+    int populacao1, pontoturistico1;
+
+    //Declaração de variáveis tipo float 
+    float areakm1, pib1, densidadepopulacional1, pibpercapita1;
 
     //Declaração de variáveis: Carta 2
-    //Declaração de variáveis do tipo char (Strings ou caracteres únicos)
-    char estado2 = ' ', codigo2[4] = "A01", cidade2[20] = " ";
-    //Declaração de variáveis do tipo int (Números inteiros)
-    int populacao2 = 0, pontoturistico2 = 0;
-    //Declaração de variáveis do tipo float (Números de ponto flutuante)
-    float areakm2 = 0.0, pib2 = 0.0;
+    
+    //Declaração de variáveis tipo char
+    char estado2, codigo2[4], cidade2[20];
+
+    //Declaração de variáveis tipo int 
+    int populacao2, pontoturistico2;
+
+    //Declaração de variáveis tipo float
+    float areakm2, pib2, densidadepopulacional2, pibpercapita2;
+
 
     //Coleta de dados: Carta 1
+
     printf("Insira As Informações Correspondentes da Carta 1: \n");
 
     printf("Insira o Estado: \n");
-    //Requisição de dado de caractere único através do especificador %c para ser armazenado em estado1
+    //Requisição de dado de caractere único pelo especificador %c para armazenar em estado1
     scanf(" %c", &estado1);
 
     printf("Insira o Código: \n");
-    //O mesmo que acima para suas referentes variáveis e especificadores
     scanf("%s", codigo1);
 
     printf("Insira o Nome da Cidade: \n");
@@ -49,6 +56,7 @@ int main(){
 
 
     //Coleta de dados: Carta 2
+
     printf("Insira As Informações Correspondentes da Carta 2: \n");
 
     printf("Insira o Estado: \n");
@@ -74,6 +82,24 @@ int main(){
 
     printf("\n");
 
+        //Cálculos: Carta 1
+
+        //Cálculo de Densidade Populacional com Casting
+        densidadepopulacional1 = (float) populacao1 / areakm1;
+
+        //Cálculo de PIB per Capita
+        pibpercapita1 = (float) pib1 / populacao1;
+
+
+        //Cálculos: Carta 2
+
+        //Cálculo de Densidade Populacional com Casting
+        densidadepopulacional2 = (float) populacao2 / areakm2;
+
+        //Cálculo de PIB per Capita
+        pibpercapita2 = (float) pib2 / populacao2;
+        
+
 
     //Amostra dos Dados Coletados: Carta 1
     printf("Carta 1:\n");
@@ -81,9 +107,11 @@ int main(){
     printf("Código: %s \n", codigo1);
     printf("Nome da Cidade: %s \n", cidade1);
     printf("População: %d \n", populacao1);
-    printf("Área: %f km² \n", areakm1);
-    printf("PIB: %f bilhões de reais \n", pib1);
+    printf("Área: %.2f km² \n", areakm1);
+    printf("PIB: %.2f reais \n", pib1);
     printf("Números de Pontos Turísticos: %d \n", pontoturistico1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadepopulacional1);
+    printf("PIB per Capita: %.2f reais\n", pibpercapita1);
 
     printf("\n");
 
@@ -93,14 +121,14 @@ int main(){
     printf("Código: %s \n", codigo2);
     printf("Nome da Cidade: %s \n", cidade2);
     printf("População: %d \n", populacao2);
-    printf("Área: %f km² \n", areakm2);
-    printf("PIB: %f bilhões de reais \n", pib2);
+    printf("Área: %.2f km² \n", areakm2);
+    printf("PIB: %.2f reais \n", pib2);
     printf("Números de Pontos Turísticos: %d \n", pontoturistico2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadepopulacional2);
+    printf("PIB per Capita: %.2f reais\n", pibpercapita2);
 
 
-    
     return 0;
     
-
 
 }
